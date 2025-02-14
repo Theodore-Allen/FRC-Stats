a1c24ecc3e25bb1e8147dad3efac35387234adcc
Theodore-Allen <124205027+Theodore-Allen@users.noreply.github.com>
2025-02-13 17:39:44 -0800

    Team Obj worked on not complete, weird spacing
    pls fix guys

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
+// Global Variables
 let globolData = [];
 let SortedList = [];
 let selectElemts = document.getElementById('dropdown'); // will probaly be changed to a class soon
 
 let teamcontainer = document.getElementById('team');
 let dropDownContainer = document.getElementById('dropdown-container');
-let dropBtn = document.getElementById("addDrop")
+let dropBtn = document.getElementById('addDrop');
+
+// API Keys
+const API_KEY =
+  'Ym1UYQf6f7egblFtsTNkieAVJWVhSYrnva9rHVArhtqBPSbjMdDKb2rTer8491go'; // theodore nigga please dont clone the api key to the repo
 
 // run
-
 async function app() {
+  console.log('heklo world');
   const options = getAllVariables();
   // sets up the boilerPlate EL
   options.forEach((e) => {
     addOption(e);
   });
   initDropDown();
-  
+
   // add event listener
-    dropBtn.addEventListener('click', () =>
-    {
-      initDropDown();
-    })
+  dropBtn.addEventListener('click', () => {
+    initDropDown();
+  });
   // globolData.forEach()
 
   console.log(calculateAverage(987, 'Pickup_Time'));
   console.log(globolData);
 }
 
-//intailizing
+// Fetches the JSON
 fetch('/Data.json')
   .then((e) => {
     return e.json();
@@ -41,7 +45,6 @@ fetch('/Data.json')
   });
 
 // Funcions
-
 function calculateAverage(teamNumber, variable) {
   // Find the team number by matching the team number
   const team = getTeam(teamNumber);
@@ -60,6 +63,7 @@ function calculateAverage(teamNumber, variable) {
   const sum = values.reduce((acc, val) => acc + val, 0);
   return sum / values.length;
 }
+
 function getAllVariables() {
   const allVariables = new Set();
 
@@ -71,6 +75,7 @@ function getAllVariables() {
 
   return Array.from(allVariables);
 }
+
 function getTeam(teamNumber) {
   const team = globolData.find((team) => team.team === teamNumber);
   if (!team) {
@@ -79,12 +84,14 @@ function getTeam(teamNumber) {
   }
   return team;
 }
+
 function addOption(optionName) {
   let option = document.createElement('option');
   option.value = optionName;
   option.text = optionName;
   selectElemts.appendChild(option);
 }
+
 function initDropDown() {
   let clone = selectElemts.cloneNode(true);
   let div = document.createElement('div');
@@ -95,3 +102,31 @@ function initDropDown() {
   div.appendChild(clone);
   dropDownContainer.appendChild(div);
 }
+
+// fetching outside data
+
+function apiStatus() {
+  const apiTestUrl = 'https://www.thebluealliance.com/api/v3/status';
+
+  fetch(apiTestUrl, {
+    method: 'GET',
+    headers: {
+      'X-TBA-Auth-Key': API_KEY,
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error('Error:', error));
+}
+
+// add the team rending
+// which js makes all the teams show up based off the filtered list '
+
+
+// and pls 
+function renderTeams() {}
+
+
+
+
